da6a566bd8fdc473817c7e5e53f5bd57f3d2cd89
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2024-02-29 00:12:11 +0100

    feat: add example of web-component dynamic injection

---
+++ dynamic-injection/index.js
@@ -0,0 +1,51 @@
+let appModule = null;
+
+const appContainer = document.querySelector('#container');
+const injectAppBtn = document.querySelector('#inject-app');
+
+const appLoader = document.createElement('h3');
+appLoader.style.color = 'red';
+appLoader.textContent = `-------------- Loading app... --------------`;
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const onAppClose = () => {
+  appContainer.childNodes.forEach((node) => node.remove());
+  injectAppBtn.disabled = false;
+  appContainer.append(injectAppBtn);
+};
+
+const importAppModule = async () => {
+  appContainer.append(appLoader);
+  await wait(2000);
+  let module = null;
+  try {
+    module = await import('./kk-application.js');
+  } catch (_e) {
+    console.log(`Couldn't load app`);
+  } finally {
+    appLoader.remove();
+  }
+  return module.KkApplication;
+};
+
+const injectApp = () => {
+  injectAppBtn.remove();
+  const appToInject = new appModule();
+  appToInject.setOnAppCloseCallback(onAppClose);
+  appToInject.setExternalCallback(() => {
+    alert('Callback passed as parameter');
+  });
+  appContainer.append(appToInject);
+};
+
+injectAppBtn.addEventListener('click', async () => {
+  injectAppBtn.disabled = true;
+  if (appModule === null) {
+    appModule = await importAppModule();
+  }
+  injectApp();
+  injectAppBtn.disabled = false;
+});
---
+++ dynamic-injection/kk-application.js
@@ -0,0 +1,55 @@
+const template = `
+<section>
+  <h3>Injected application</h3>
+  <button id="btn-1">Internal API</button>
+  <button id="btn-2">Call passed function</button>
+  <button id="btn-3">Close</button>
+</section>
+`;
+
+export class KkApplication extends HTMLElement {
+  static TAG = 'kk-application';
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = template;
+    this.setElementsReferences();
+    this.externalFunction = null;
+    this.onAppClose = null;
+  }
+
+  connectedCallback() {
+    this.setEventListeners();
+  }
+
+  setElementsReferences() {
+    this.btn1 = this.shadowRoot.querySelector('#btn-1');
+    this.btn2 = this.shadowRoot.querySelector('#btn-2');
+    this.btn3 = this.shadowRoot.querySelector('#btn-3');
+  }
+
+  setEventListeners() {
+    this.btn1.addEventListener('click', () => {
+      alert('Called an internal method');
+    });
+  }
+
+  setExternalCallback(callback) {
+    if (this.externalFunction !== null) {
+      this.btn2.removeEventListener('click', this.externalFunction);
+    }
+    this.externalFunction = callback;
+    this.btn2.addEventListener('click', callback);
+  }
+
+  setOnAppCloseCallback(callback) {
+    if (this.onAppClose !== null) {
+      this.btn3.removeEventListener('click', this.onAppClose);
+    }
+    this.onAppClose = callback;
+    this.btn3.addEventListener('click', callback);
+  }
+}
+
+customElements.define(KkApplication.TAG, KkApplication);
